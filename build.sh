57ce6ceba8290e5147db937b381548e6888b4eb4
Warren Roberts <16521890+warren-roberts@users.noreply.github.com>
2026-07-22 23:27:16 -0500

    Align Cloudflare build with Hugo and Congo updates

--- build.sh
+++ build.sh
@@ -10,14 +10,16 @@
 main() {
 
   DART_SASS_VERSION=1.93.2
-  GO_VERSION=1.25.3
-  HUGO_VERSION=0.152.2
+  GO_VERSION=1.26.5
+  HUGO_VERSION=0.164.0
   NODE_VERSION=22.20.0
 
   export TZ=Europe/Oslo
+  mkdir -p "${HOME}/.local"
 
   # Install Dart Sass
   echo "Installing Dart Sass ${DART_SASS_VERSION}..."
+  rm -rf "${HOME}/.local/dart-sass"
   curl -sLJO "https://github.com/sass/dart-sass/releases/download/${DART_SASS_VERSION}/dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
   tar -C "${HOME}/.local" -xf "dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
   rm "dart-sass-${DART_SASS_VERSION}-linux-x64.tar.gz"
@@ -25,6 +27,7 @@ main() {
 
   # Install Go
   echo "Installing Go ${GO_VERSION}..."
+  rm -rf "${HOME}/.local/go"
   curl -sLJO "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
   tar -C "${HOME}/.local" -xf "go${GO_VERSION}.linux-amd64.tar.gz"
   rm "go${GO_VERSION}.linux-amd64.tar.gz"
@@ -32,8 +35,9 @@ main() {
 
   # Install Hugo
   echo "Installing Hugo ${HUGO_VERSION}..."
+  rm -rf "${HOME}/.local/hugo"
   curl -sLJO "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
-  mkdir "${HOME}/.local/hugo"
+  mkdir -p "${HOME}/.local/hugo"
   tar -C "${HOME}/.local/hugo" -xf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
   rm "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
   export PATH="${HOME}/.local/hugo:${PATH}"
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module wrnet-web
 
-go 1.25.4
+go 1.26.5
 
 require github.com/jpanther/congo/v2 v2.14.0 // indirect
